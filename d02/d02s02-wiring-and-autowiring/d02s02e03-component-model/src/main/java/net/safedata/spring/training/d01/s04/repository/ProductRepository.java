2fb25502ae28e4f770527a8d2587a119a690f855
Bogdan Solga <bogdan.solga@gmail.com>
2018-10-01 20:57:20 +0300

    [improve] Added an in-memory list of products

--- d02/d02s02-wiring-and-autowiring/d02s02e03-component-model/src/main/java/net/safedata/spring/training/d01/s04/repository/ProductRepository.java
+++ d02/d02s02-wiring-and-autowiring/d02s02e03-component-model/src/main/java/net/safedata/spring/training/d01/s04/repository/ProductRepository.java
@@ -1,6 +1,10 @@
 package net.safedata.spring.training.d01.s04.repository;
 
+import net.safedata.spring.training.domain.model.Product;
 import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple product repository
@@ -10,7 +14,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ProductRepository {
 
+    private List<Product> products = Arrays.asList(
+            new Product(1, "an iSomething", 50),
+            new Product(1, "an iSomething else", 20)
+    );
+
     public void displayProducts() {
-        System.out.println("Displaying all the products, from the database [a little later]...");
+        System.out.println("Displaying all the products:");
+        for (Product product : products) {
+            System.out.println("\t" + product.toString());
+        }
     }
 }
